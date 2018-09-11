c225e00db1d3532071dc902aa111297eb6064d72
Loree Nichols <loree.d.nichols@gmail.com>
2018-09-11 17:35:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ function currentLine(x) {
   var line = []
   if (x.length === 0) {
     return "The line is currently empty."
-  } else { for(var i = 0; i < x.length; i++){
+  } else { 
+    for(var i = 0; i < x.length; i++){
     line += (i + 1) + "." + x[i] + ","
   }
   line = line.slice(0, line.length-2)
   return "The line is currently: " + line
 }
-function takeANumber (line, name) {
-  var line = 
-  var name = 
 }
-return ("welcome" + [name]". You are number"[] "in line."
+function takeANumber (katzDeliLine, name) {
+ katzDeliLine.push(name);
+return "Welcome" + name + ". You are number" + katzDeliLine.length "in line.";}
