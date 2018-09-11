73294c06863cbda4f3cf4ae15bd10264cc084bb7
Loree Nichols <loree.d.nichols@gmail.com>
2018-09-11 17:40:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,4 +13,14 @@ function currentLine(x) {
 }
 function takeANumber (katzDeliLine, name) {
  katzDeliLine.push(name)
-return "Welcome" + name + ". You are number" + katzDeliLine.length "in line."}
+return "Welcome" + name + ". You are number" + katzDeliLine.length "in line."
+  }
+  function nowServing(x) {
+    if (x.length === 0) {
+      return "There is nobody waiting to be served!"
+    }else{
+      var name = x[0];
+      x.splice(0, 1);
+      return "Currently serving" + name + ".";
+    }
+  }
