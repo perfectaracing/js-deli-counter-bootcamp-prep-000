f2e8dce4e7deda820c3776c98598db9344b57151
Loree Nichols <loree.d.nichols@gmail.com>
2018-09-11 17:30:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+var katzDeliLine = [];
+function currentLine(x) {
+  var line = []
+  if (x.length === 0) {
+    return "The line is currently empty."
+  } else { for(var i = 0; i < x.length; i++){
+    line += (i + 1) + "." + x[i] + ","
+  }
+  line = line.slice(0, line.length-2)
+  return "The line is currently: " + line
+}
 function takeANumber (line, name) {
   var line = 
   var name = 
