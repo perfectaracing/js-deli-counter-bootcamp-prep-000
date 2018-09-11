48a3b8159f4db8e35844892ba34fbd229c29c751
Loree Nichols <loree.d.nichols@gmail.com>
2018-09-11 17:41:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ function currentLine(x) {
 }
 }
 function takeANumber (katzDeliLine, name) {
- katzDeliLine.push(name)
-return "Welcome" + name + ". You are number" + katzDeliLine.length "in line."
+ katzDeliLine.push(name);
+return "Welcome," + name + ". You are number" + katzDeliLine.length "in line."
   }
   function nowServing(x) {
     if (x.length === 0) {
